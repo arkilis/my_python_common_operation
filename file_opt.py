9bb09164c1da85cd6215280245ee63acaa75f99b
Ben LIU <arkilis@gmail.com>
2013-06-29 11:38:46 +1000

    add more read functions

--- file_opt.py
+++ file_opt.py
@@ -5,10 +5,10 @@
 
 
 # Read a file line by line
-# Remember in this way, the output will keep the '\n' of each line
+# Remember in this way, but the output will keep the '\n' of each line
 import os
 
-def readfile(filePath):
+def readfile1(filePath):
     szList=[]
     if(os.path.exists(filePath)==False):
         return False
@@ -21,7 +21,38 @@ def readfile(filePath):
     return szList
 
 
-# 
+# To avoid the problem, there are two ways
+# The 1st is use readlines()
+# But this will consume a lot memory in practise
+
+def readfile2(filePath):
+    if(os.path.exists(filePath)==False):
+        return False
+    fp= open(filePath, "r")
+    for line in lines:
+        print line
+    fp.close()
+
+# 2nd way to solve this to use the replace(), similar with readfile1()
+
+def readfile3(filePath):
+    szList=[]
+    if(os.path.exists(filePath)==False):
+        return False
+    fp= open(filePath, "r")
+    r= fp.readline()
+    while(r!='\n'):
+        print r.replace("\n", "")
+        r= fp.readline()
+        szList.append(r)
+    return szList
+
+# Read all the content of a file
+def readAll(filePath):
+    if(os.path.exists(filePath)==False):
+        return False
+    fp= open(filePath, "r") # Support only after python 2.2
+    print fp
 
 
 
